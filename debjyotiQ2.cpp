da6f83793490d4a550171ed0267a2630f35d0806
Debjyoti-25163 <debjyoti25163@iiitd.ac.in>
2025-08-28 23:45:16 +0530

    pushing  my updated  files

--- debjyotiQ2.cpp
+++ debjyotiQ2.cpp
@@ -1,3 +1,5 @@
+#include "basicIO.h"
+
 int main() {
     return 0;
 }
--- debjyotiQ2.sh
+++ debjyotiQ2.sh
@@ -1,2 +1,11 @@
-g++ -nostdlib -nostartfiles -o debjyotiQ2 debjyotiQ2.cpp
+# Compile C++ code without standard libraries
+g++ -c basicIO.cpp -o basicIO.o -fno-exceptions -fno-rtti -fno-stack-protector -nostdlib -nodefaultlibs
 
+# Assemble syscall wrapper
+nasm -f elf64 syscall.S -o syscall.o
+
+# Compile main_Q3.cpp to object file
+g++ -c debjyotiQ2.cpp -o debjyotiQ2.o -fno-exceptions -fno-rtti -fno-stack-protector -nostdlib -nodefaultlibs
+
+# Link everything together
+g++ debjyotiQ2.o basicIO.o syscall.o -o debjyotiQ2 -nostdlib -nodefaultlibs -fno-exceptions -fno-rtti -fno-stack-protector
